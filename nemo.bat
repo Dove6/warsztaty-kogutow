6238b01961193ed1829c196f4deacb655e69a89e
Dawid <24943032+Dove6@users.noreply.github.com>
2026-01-21 04:16:10 +0100

    Update game starting script
    Add compatibility settings

--- nemo.bat
+++ nemo.bat
@@ -1,1 +1,5 @@
-@.\nemo.exe /window
+@rem Ustawia dla gry kompatybilność wsteczną z 16-bitowym trybem kolorów
+@reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%cd%\nemo.exe" /t REG_SZ /d "~ 16BITCOLOR" /f >NUL
+
+@rem Włącza grę w okienku
+@start .\nemo.exe /window
